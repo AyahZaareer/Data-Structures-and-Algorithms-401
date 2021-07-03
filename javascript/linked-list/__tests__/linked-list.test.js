cbab16c0d57bdd4ba180d6d2b63b07bc7af8ceb0
AyahZaareer <ayah@LTUCSTUDENT-L075.localdomain>
2021-07-04 00:20:31 +0300

    5 done

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedLilst = require('..');
 
 // describe('Linked List', () => {
 //   it('works', () => {
